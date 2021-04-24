ad07aae7f2ab37216b98ac6d62bc31451613a3d2
Andres Sainz <andresmsainz@gmail.com>
2021-04-24 19:09:19 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int x = 0;
+            int y = 0;
+
+            Console.WriteLine("Enter No. 1");
+            x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter No. 2");
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                Console.WriteLine(x + " is greater than " + y);
+            }
+            else if (x < y)
+            {
+                Console.WriteLine(x + " is less than " + y);
+            }
+            else if (x == y)
+            {
+                Console.WriteLine(x + " is equal than " + y);
+            }
+
+
+
+
+        }
+    }
+}
